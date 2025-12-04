bbab255872192f310433382b20543b6015eba803
kjjuan <koojiajuan123@gmail.com>
2025-12-04 12:00:58 +0800

    reporting pipeline

--- egt309/src/egt309/pipeline_registry.py
+++ egt309/src/egt309/pipeline_registry.py
@@ -2,6 +2,7 @@
 from kedro.pipeline import Pipeline
 from egt309.pipelines import data_prep as dp
 from egt309.pipelines import data_science as ds
+from egt309.pipelines import reporting as rp
 
 def register_pipelines() -> dict[str, Pipeline]:
     """Register the project's pipelines.
@@ -13,12 +14,14 @@ def register_pipelines() -> dict[str, Pipeline]:
     # Define the Pipeline Objects
     data_prep_pipeline = dp.create_pipeline()
     data_science_pipeline = ds.create_pipeline()
+    reporting_pipeline = rp.create_pipeline()
 
     return {
         # egister the Default Pipeline (runs everything)
-        "__default__": data_prep_pipeline + data_science_pipeline, 
+        "__default__": data_prep_pipeline + data_science_pipeline + reporting_pipeline,
         
         # Register the Individual Pipelines for targeted runs
         "data_prep": data_prep_pipeline,
-        "data_science": data_science_pipeline
+        "data_science": data_science_pipeline,
+        "reporting": reporting_pipeline
     }
